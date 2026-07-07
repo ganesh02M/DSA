a59506096d257d66b3bff73b50e625ece3fc023a
Ganesh Mishra <mishraganesh9305@gmail.com>
2026-07-07 23:09:12 +0530

    Time: 4 ms (82.36%) | Memory: 46.7 MB (16.28%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+//        if(s.length()!=t.length())
+//        return false;
+
+//        int[] count = new int[26];
+
+//        for(int i = 0 ; i<s.length();i++){
+//         count[s.charAt(i) - 'a']++;
+//          count[t.charAt(i) - 'a']--;
+
+//        }
+//        for(int c : count){
+//         if(c != 0) 
+//         return false;
+//        }
+//        return true;
+//     }
+// }
+ if(s.length() != t.length())
+            return false;
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.equals(sArr, tArr);
+    }
+}
