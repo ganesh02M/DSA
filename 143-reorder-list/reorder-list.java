274cc3bfa9a8380a82669d1a2075c350358d06e9
Ganesh Mishra <mishraganesh9305@gmail.com>
2026-07-24 22:46:59 +0530

    Time: 2 ms (87.06%) | Memory: 49.1 MB (87.29%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public void reorderList(ListNode head) {
+
+        if (head == null || head.next == null)
+            return;
+
+        // Step 1 : Find Middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2 : Reverse Second Half
+        ListNode prev = null;
+        ListNode curr = slow.next;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // First half ko alag karo
+        slow.next = null;
+
+        // Step 3 : Merge
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
