89af58779075772a3005ada340cf6b3197d7ea67
Ganesh Mishra <mishraganesh9305@gmail.com>
2026-08-13 08:17:26 +0530

    Time: 134 ms (57.10%) | Memory: 43.3 MB (14.39%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -9,7 +9,7 @@ private boolean dfs(char[][] board, int r, int c, String word, int idx) {
     if (idx == word.length()) return true;
     if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != word.charAt(idx)) return false;
     char temp = board[r][c];
-    board[r][c] = '#'; // mark visited
+    board[r][c] = '#'; 
     boolean found = dfs(board, r+1, c, word, idx+1) || dfs(board, r-1, c, word, idx+1)
                  || dfs(board, r, c+1, word, idx+1) || dfs(board, r, c-1, word, idx+1);
     board[r][c] = temp;
