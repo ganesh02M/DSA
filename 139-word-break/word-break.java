700c977712cefd701971c2ffd0ea4acf04e34130
Ganesh Mishra <mishraganesh9305@gmail.com>
2026-08-29 11:09:26 +0530

    Time: 8 ms (48.96%) | Memory: 46.2 MB (47.29%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -5,7 +5,10 @@ class Solution {
     dp[0] = true;
     for (int i = 1; i <= s.length(); i++) {
         for (int j = 0; j < i; j++) {
-            if (dp[j] && dict.contains(s.substring(j, i))) { dp[i] = true; break; }
+            if (dp[j] && dict.contains(s.substring(j, i)))
+             { dp[i] = true; 
+             break; 
+             }
         }
     }
     return dp[s.length()];
